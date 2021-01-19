acaadb8c154f52e5741d7daedc5b90ab8fccf109
swapnil2725 <swapnil2725@gmail.com>
2021-01-19 16:30:27 +0100

    Update build.sh

--- openstack/loci/build.sh
+++ openstack/loci/build.sh
@@ -161,7 +161,7 @@ function get_project_image_build_arguments {
 #
 # BASE_IMAGE represents LOCI's "base" image name.
 # Use ubuntu|leap15|centos|debian to build base image from LOCI's Dockerfiles.
-BASE_IMAGE=${BASE_IMAGE:-"gcr.io/google_containers/ubuntu-slim:0.14"}
+BASE_IMAGE=${BASE_IMAGE:-"ubuntu:bionic"}
 # Replace with Registry URI with your registry like your
 # dockerhub user. Example: "docker.io/openstackhelm"
 REGISTRY_URI=${REGISTRY_URI:-"172.17.0.1:5000/openstackhelm/"}
@@ -170,7 +170,7 @@ VERSION=${VERSION:-"latest"}
 # The openstack branch to build, if no per project branch is given.
 OPENSTACK_VERSION=${OPENSTACK_VERSION:-"master"}
 # Sepcify OS distribution
-DISTRO=${DISTRO:-"ubuntu_xenial"}
+DISTRO=${DISTRO:-"ubuntu_bionic"}
 # extra build arguments for the base image. See loci's dockerfiles for
 # arguments that could be used for example.
 base_extra_build_args=${base_extra_build_args:-"--force-rm --pull --no-cache"}
